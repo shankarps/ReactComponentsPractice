45ffb04c1383cbf2e59b96a55e02105a8148917a
SPachari <shankar.pachari@internetbrands.com>
2019-12-05 18:20:17 -0800

    Component child reuse

--- src/index.js
+++ src/index.js
@@ -2,31 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import Comment from "./Comment";
+import CommentWithButtons from "./CommentWithButtons";
 
 //Define the App component
 //Functional component
 const App = () => {
   return (
     <div className="ui container comments">
-      {/* Nested component*/}
-      <Comment
-        author={faker.name.findName()}
-        comment={faker.lorem.sentence()}
-        time="Today at 6 PM"
-        imageSrc={faker.image.avatar()}
-      />
-      <Comment
-        author="Peter Lynch"
-        comment="Price is what you pay. Value is what you get."
-        time="Today at 7 PM"
-        imageSrc={faker.image.avatar()}
-      />
-      <Comment
-        author="Warren Buffett"
-        comment="Price is what you pay. Value is what you get."
-        time="Today at 8.30 PM"
-        imageSrc={faker.image.avatar()}
-      />
+      <CommentWithButtons>
+        {/* Nested component*/}
+        <Comment
+          author={faker.name.findName()}
+          comment={faker.lorem.sentence()}
+          time="Today at 6 PM"
+          imageSrc={faker.image.avatar()}
+        />
+      </CommentWithButtons>
+      <CommentWithButtons>
+        <Comment
+          author="Peter Lynch"
+          comment="Price is what you pay. Value is what you get."
+          time="Today at 7 PM"
+          imageSrc={faker.image.avatar()}
+        />
+      </CommentWithButtons>
+      <CommentWithButtons>
+        <Comment
+          author="Warren Buffett"
+          comment="Price is what you pay. Value is what you get."
+          time="Today at 8.30 PM"
+          imageSrc={faker.image.avatar()}
+        />
+      </CommentWithButtons>
     </div>
   );
 };
